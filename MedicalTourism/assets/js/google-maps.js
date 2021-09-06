7b1ddf71221164e5d346f2b2e938ab813ffc8992
HarshSrivastava07 <65861642+HarshSrivastava07@users.noreply.github.com>
2021-09-07 04:33:50 +0530

    Add files via upload

---
+++ MedicalTourism/assets/js/google-maps.js
@@ -0,0 +1,15 @@
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 12.9716, lng: 77.5946 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 12,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
+
